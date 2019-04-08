e30baa5ac9834be7e9d1587c6538fc0b0f37ce46
Stefan Einarsson <stefane81@gmail.com>
2019-04-08 15:00:18 +0200

    First push

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>Hi, I am God.</h1>
+				<Person name="Stefan" age="37">
+					My hobbies: CS
+				</Person>
+				<Person name="Bergros" age="33" />
+				<Person name="Einar Bragi" age="4" />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const person = props => {
+	return (
+		<div>
+			<p>
+				My name is {props.name} and I am {props.age} years old!
+			</p>
+			<p>{props.children}</p>
+			<hr />
+		</div>
+	);
+};
+
+export default person;
