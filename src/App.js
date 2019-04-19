4a4bc454891cd1580252779ec4496e6cd88077ba
Stefan Einarsson <stefane81@gmail.com>
2019-04-19 21:38:21 +0200

    not sure

--- src/App.js
+++ src/App.js
@@ -61,10 +61,21 @@ class App extends Component {
 	};
 
 	render() {
+		const style = {
+			backgroundColor: 'white',
+			font: 'inherit',
+			border: '1px solid blue',
+			padding: '8px',
+			cursor: 'pointer'
+		};
+
 		return (
 			<div className="App">
 				<h1>Hi, I am God.</h1>
-				<button onClick={this.switchNameHandler.bind(this, 'Not real')}>
+				<button
+					style={style}
+					onClick={this.switchNameHandler.bind(this, 'Not real')}
+				>
 					Switch Name
 				</button>
 				<Person
