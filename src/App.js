27b9a49d2d6abe2c1555eef3ed77075cef2a766b
Stefan Einarsson <stefane81@gmail.com>
2019-05-13 09:25:02 +0200

    Dynamically add CSS classes to html elements

--- src/App.js
+++ src/App.js
@@ -90,9 +90,19 @@ class App extends Component {
       style.backgroundColor = "red";
     }
 
+    const classes = [];
+
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
+    }
+
+
     return (
       <div className="App">
-        <h1>Hi, I am ok.</h1>
+        <p className={classes.join(' ')}>Hi, I am ok.</p>
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
