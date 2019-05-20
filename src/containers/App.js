d9962a7af2405b07a08b004693af7d2bc814f4ed
Stefan Einarsson <stefane81@gmail.com>
2019-05-20 14:23:08 +0200

    Reorginized file  structure now working as before

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from './App.module.css';
 import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
 
 class App extends Component {
 	state = {
@@ -66,17 +67,11 @@ class App extends Component {
 		if (this.state.showPersons) {
 			persons = (
 				<div>
-					{this.state.persons.map((person, index) => {
-						return (
-							<Person
-								key={person.id}
-								name={person.name}
-								age={person.age}
-								click={() => this.deletePersonHandler(index)}
-								changed={event => this.nameChangedHandler(event, person.id)}
-							/>
-						);
-					})}
+					<Persons
+						persons={this.state.persons}
+						clicked={this.deletePersonHandler}
+						changed={this.nameChangedHandler}
+					/>
 				</div>
 			);
 
